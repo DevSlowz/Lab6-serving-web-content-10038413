1c23444bfd1b72600378b8a39d2bbf327f96a942
DevSlowz <91501848+DevSlowz@users.noreply.github.com>
2023-12-13 10:32:57 -0500

    Complete

---
+++ src/main/java/com/example/servingwebcontent100838413/ServingWebContent100838413Application.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100838413;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContent100838413Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContent100838413Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servingwebcontent100838413/ServingWebContent100838413ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent100838413;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServingWebContent100838413ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
